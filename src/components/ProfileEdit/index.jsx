50c7e89786d0fee29a17a8d720813c4bac503044
Ahmeed-Sayed <ahmeedsayed01@gmail.com>
2023-11-06 20:05:27 +0200

    adjusted footer, added dummy product data and made card more generalized

--- src/components/ProfileEdit/index.jsx
+++ src/components/ProfileEdit/index.jsx
@@ -1,13 +1,9 @@
-import Footer from "../Home/components/Footer";
-import Header from "../Home/components/Header";
 import Edit from "./Edit";
 
 const ProfileEdit = () => {
   return (
     <>
-      <Header />
       <Edit />
-      <Footer />
     </>
   );
 };
