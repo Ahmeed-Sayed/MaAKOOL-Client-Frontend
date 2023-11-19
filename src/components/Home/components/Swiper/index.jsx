95ea9cd2d6ec6af26e19c3a0e43bf1405dd6f3fa
Ahmeed-Sayed <ahmeedsayed01@gmail.com>
2023-11-19 02:01:08 +0200

    adjusted home page and not found

--- src/components/Home/components/Swiper/index.jsx
+++ src/components/Home/components/Swiper/index.jsx
@@ -6,8 +6,29 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 import "./styles.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function AppSwiper() {
+  const [offers, setOffers] = useState([]);
+
+  const fetchOffers = async () => {
+    try {
+      const response = await axios.get(
+        "http://127.0.0.1:8000/resturant/offer/"
+      );
+      console.log(response.data);
+      setOffers(response.data);
+    } catch (error) {
+      console.error("Error during fetching offers:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchOffers();
+  }, []);
+
   return (
     <>
       <div className="container swip mt-5 d-flex align-items-center justify-content-center">
@@ -25,26 +46,21 @@ export default function AppSwiper() {
           modules={[Autoplay, Pagination, Navigation]}
           className="mySwiper"
         >
-          <SwiperSlide>
-            <div className="d-flex justify-content-center">
-              <img
-                src="https://swiperjs.com/demos/images/nature-3.jpg"
-                alt="slide-1"
-                className="rounded"
-                style={{ width: "500px", height: "400px" }}
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="d-flex justify-content-center">
-              <img
-                src="https://swiperjs.com/demos/images/nature-4.jpg"
-                alt="slide-2"
-                className="rounded"
-                style={{ width: "500px", height: "400px" }}
-              />
-            </div>
-          </SwiperSlide>
+          {offers.length > 0 &&
+            offers.map((offer, index) => (
+              <SwiperSlide key={index}>
+                <div className="d-flex justify-content-center">
+                  <Link to={"browse/"}>
+                    <img
+                      src={`http://127.0.0.1:8000/${offer.image}`}
+                      alt={`slide-${index + 1}`}
+                      className="rounded"
+                      style={{ width: "500px", height: "400px" }}
+                    />
+                  </Link>
+                </div>
+              </SwiperSlide>
+            ))}
         </Swiper>
       </div>
     </>
